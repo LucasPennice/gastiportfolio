29fd65949ad4d46b9f8825480da3078a40f88094
Lucas Pennice <lucaspennice@gmail.com>
2022-07-01 17:07:42 -0300

    contact me more spacing

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
 			<ul>
 				{contactData.map((contact, contactIdx) => {
 					return (
-						<li key={contactIdx} className='flex pl-5 mb-5 text-3xl border-l-4 border-celesteOscuro'>
+						<li key={contactIdx} className='flex pl-5 mb-12 text-3xl font-light border-l-4 border-celesteOscuro'>
 							<span className='flex items-end mr-5'>{contact.Icon}</span>
 							{contact.text}
 						</li>
